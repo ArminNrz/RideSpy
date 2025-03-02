a54564150cf9415046c8ba5cf39dd341cd8ea31d
armin norozian <armin.norozian@snapp.cab>
2025-03-02 14:24:35 +0330

    first commit

---
+++ src/main/java/com/example/ridespy/RideSpyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ridespy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSpyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideSpyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ridespy/RideSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ridespy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideSpyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
